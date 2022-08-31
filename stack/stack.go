c9585f695119814d2ce9d50daf51e61a86b9a99c
0xMarvell <rokemarvellous@gmail.com>
2022-08-31 15:15:39 +0100

    implemented stack and stack Pop method

--- stack/stack.go
+++ stack/stack.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+type Stack []string
+
+func (s *Stack) IsEmpty() bool {
+	return len(*s) == 0
+}
+
+func (s *Stack) Push(str string) {
+	*s = append(*s, str)
+	fmt.Println(*s)
+}
+
+// func (s *Stack) Pop() (string, bool) {
+
+// }
+
 func main() {
-	fmt.Println("stack")
+	var stack Stack
+	stack.Push("D")
+	stack.Push("S")
+	stack.Push("A")
 }
