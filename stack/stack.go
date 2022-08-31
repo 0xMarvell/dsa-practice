6c33436188a44e5236a561e7e6694066f499cab2
0xMarvell <rokemarvellous@gmail.com>
2022-08-31 15:31:07 +0100

    added stack Pop method

--- stack/stack.go
+++ stack/stack.go
@@ -13,13 +13,24 @@ func (s *Stack) Push(str string) {
 	fmt.Println(*s)
 }
 
-// func (s *Stack) Pop() (string, bool) {
+func (s *Stack) Pop() (string, bool) {
+	if s.IsEmpty() {
+		return "", false
+	} else {
+		idx := len(*s) - 1
+		element := (*s)[idx]
+		*s = (*s)[:idx]
 
-// }
+		fmt.Println(*s)
+		return element, true
+	}
+}
 
 func main() {
 	var stack Stack
 	stack.Push("D")
 	stack.Push("S")
 	stack.Push("A")
+
+	stack.Pop()
 }
